f7690664cd76cdc4ae11f1d2d07115ef2b38a3b6
AnderzRomero <andersonromerorico@gmail.com>
2023-03-24 22:00:26 -0500

    Finaliza ajuste del algoritmo

--- algoritmo.py
+++ algoritmo.py
@@ -4,52 +4,57 @@ import matplotlib.pyplot as plt
 import folium
 import haversine
 
+
 cities = []
 
 with open('ciudades.csv', newline='') as csvfile:
       reader = csv.reader(csvfile, delimiter=',')  
-
-      # for row in reader:
-      #    print(', '.join(row))
-
+      next(reader)
       for row in reader:
-             city = {'nombre': row[0], 'latitud': float(row[1]), 'longitud': float(row[2])}
-             cities.append(city)
-
-
-# print(cities)
-
-# df = pd.read_csv('D:\OneDrive - Pontificia Universidad Javeriana\Informacion\Documents\coder\Viajero\ciudades.csv') 
-
+        city = row[0]
+        latitud = float(row[1])
+        longitud = float(row[2])
+        cities.append((city, latitud, longitud))
+      print("Orden del archivo CSV")
+      print(cities, "\n")
 
 # Crear un grafo completo con todas las ciudades
 G = nx.complete_graph(len(cities))
+
 
 # Calcular las distancias entre todas las ciudades utilizando la fórmula de Haversine
 for i in range(len(cities)):
       for j in range(i+1, len(cities)):
           city1 = cities[i]
           city2 = cities[j]
-          dist = haversine(city1['latitud'], city1['longitud'], city2['latitud'], city2['longitud'])
-          G[i][j]['weight'] = dist
-
+          dist = haversine.haversine((city1[1], city1[2]), (city2[1], city2[2]))
+          G.add_weighted_edges_from([(i, j, dist)])
+          #print(G)
+          
 # Calcular la ruta óptima utilizando el algoritmo TSP
-route = nx.algorithms.approximation.traveling_salesman_problem.traveling_salesman_problem(G)
+route = nx.algorithms.approximation.traveling_salesman_problem(G)
+print("ruta optima")
+print(route,"\n")
 
 # Ordenar las ciudades según la ruta óptima
 ordered_cities = [cities[i] for i in route]
+print("ruta optima Nombres")
+print(ordered_cities,"\n")
 
 # Crear un mapa centrado en la primera ciudad de la ruta óptima
-m = folium.Map(location=[ordered_cities[0]['latitud'], ordered_cities[0]['longitud']], zoom_start=6)
+mapa = folium.Map(location=[ordered_cities[0][1], ordered_cities[0][2]], zoom_start=5)
+
 
 # Añadir marcadores para cada ciudad en la ruta óptima
-for i in range(len(ordered_cities)):
-      city = ordered_cities[i]
-      folium.Marker(location=[city['latitud'], city['longitud']], popup=city['nombre']).add_to(m)
+for city in ordered_cities:
+    folium.Marker(location=[city[1], city[2]],popup=[city[1], city[2]]).add_to(mapa)
+
 
 # Añadir una línea que conecte todas las ciudades en la ruta óptima
-locations = [(city['latitud'], city['longitud']) for city in ordered_cities]
-folium.PolyLine(locations=locations, color='blue').add_to(m)
+locations = [[city[1], city[2]] for city in ordered_cities]
+folium.PolyLine(locations=locations, weight=2.5, color='red').add_to(mapa)
+
 
 # Mostrar el mapa en pantalla
-m
+mapa.save("mapa.html")
+print(mapa)
